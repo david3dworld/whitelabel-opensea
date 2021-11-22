9de4e805193c3d57c7f76764cc2b320f1fce3238
dev420 <sspike191919@gmail.com>
2021-11-22 14:45:23 +0300

    opensea-whitelabel

---
+++ src/constants.ts
@@ -0,0 +1,9 @@
+export const ASSET_URL =
+  "https://opensea.io/assets/0x1301566b3cb584e550a02d09562041ddc4989b91/28";
+
+export enum EmbedTheme {
+  Default = "true",
+  Simple = "simple",
+}
+
+export const CHOSEN_THEME = EmbedTheme.Default;
